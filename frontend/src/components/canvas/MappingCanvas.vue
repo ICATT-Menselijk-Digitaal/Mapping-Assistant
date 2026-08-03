e5d54c643ed5eccdc77cdc09a4637b056d4c02b3
Kim <kim@icatt.nl>
2026-08-03 14:55:52 +0200

    Add TODO tag to remove temporary default URLs for source and target
    Add TODO tag to remove temporary default links for source and target URLs.

--- frontend/src/components/canvas/MappingCanvas.vue
+++ frontend/src/components/canvas/MappingCanvas.vue
@@ -80,6 +80,7 @@ function onSourceFileChange(event: Event) {
   if (file) emit('SourceFileSelected', file)
 }
 
+// TODO(production): remove temporary default link
 const sourceUrlInput = ref(
   'https://raw.githubusercontent.com/NL-AMS-LOCGOV/esuite-data-extractie/refs/heads/main/openapi-spec/OpenAPI.yaml',
 )
@@ -94,6 +95,7 @@ function onTargetFileChange(event: Event) {
   if (file) emit('TargetFileSelected', file)
 }
 
+// TODO(production): remove temporary default link
 const targetUrlInput = ref('https://openzaak.dev.kiss-demo.nl/zaken/api/v1/openapi.json')
 
 function onTargetUrlSubmit() {
